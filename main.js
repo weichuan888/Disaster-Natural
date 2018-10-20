793665d1779e191a9702b3a3d6b1664d47bbf795
weichuan <weichuan.tran@gmail.com>
2018-10-20 13:41:35 +0200

    modification nav bar

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function OpenNav() {
+    var x = document.getElementById("nav-main"); 
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+
+/* function openNav() {
+    var x = document.getElementById("nav-main");
+    x.style.width = "250px";
+}
+  
+function closeNav(){
+    document.getElementById('nav-main').style.width = "0";
+}
+ */
+    
+
